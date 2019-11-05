17485f0f20e129d2971df6934b5baaa4dfdd9b58
chrishall71 <chrishallsrq@gmail.com>
2019-11-05 17:51:31 -0600

    JS Loop added, CSS styling added
    Lesson 1:3 JS and CSS

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,10 @@ var repository = [
 		types: ['fire', 'flying']
 	},
 ];
+
+for (var i = 0; i < repository.length; i++) {
+	document.write ('<br>' + repository[i].name, ':'+'&nbsp &nbsp ' + ' Height:  ', repository[i].height);
+if (repository[i].height > 5) {
+	document.write ( ' -  Wow, that is Big! ');
+	}
+ }
