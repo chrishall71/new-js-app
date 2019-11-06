84fb7bfa088d5ff4f35ce9122bc8d3cab10479d1
chrishall71 <chrishallsrq@gmail.com>
2019-11-06 16:42:54 -0600

    use forEach function to displau pokemons
    lesson 1:4 added the forEach function to display pokemon characters

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,15 @@ var repository = [
 	},
 ];
 
-for (var i = 0; i < repository.length; i++) {
+/*for (var i = 0; i < repository.length; i++) {
 	document.write ('<br>' + repository[i].name, ':'+'&nbsp &nbsp ' + ' Height:  ', repository[i].height);
 if (repository[i].height > 5) {
 	document.write ( ' -  Wow, that is Big! ');
 	}
- }
+ }*/
+ 
+ document.write ('<h3> Pokemon Names</h3>');
+
+ Object.keys(repository).forEach(function(property) {
+	document.write(repository[property].name + '<br>' + ' Height: ' + repository[property].height + '<br>' + ' Type: ' + repository[property].types + '<br><br>');
+ });
