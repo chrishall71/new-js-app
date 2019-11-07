1d06569d873a184d3db8618c8fa49f9b5510ffea
chrishall71 <chrishallsrq@gmail.com>
2019-11-07 13:28:00 -0600

    Used IIFE in code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
-var repository = [
-	{
-		name: 'Bulbasaur', 
-		height: 2.4,
-		types: ['grass', 'poison']
-	},
-	{
-		name: 'Charmander', 
-		height: 2.0,
-		types: ['fire']
-	},
-	{
-		name: 'Charizard', 
-		height: 5.7,
-		types: ['fire', 'flying']
-	},
-];
+var pokemonRepository = (function () { //wrapped repository Array inside IIFE
+	var repository = [
+		{
+			name: 'Bulbasaur', 
+			height: 2.4,
+			types: ['grass', 'poison']
+		},
+		{
+			name: 'Charmander', 
+			height: 2.0,
+			types: ['fire']
+		},
+		{
+			name: 'Charizard', 
+			height: 5.7,
+			types: ['fire', 'flying']
+		},
+	];
+	function add (pokemon) {
+		repository.push (pokemon)
+	}
+
+	function getall() {
+		return repository;
+	}
+
+	return {
+		add: add,
+		getall: getall
+	};
+})();
 
 /*for (var i = 0; i < repository.length; i++) {
 	document.write ('<br>' + repository[i].name, ':'+'&nbsp &nbsp ' + ' Height:  ', repository[i].height);
@@ -25,6 +39,6 @@ if (repository[i].height > 5) {
  
  document.write ('<h3> Pokemon Names</h3>');
 
- repository.forEach(function(property) {
+pokemonRepository.getall ().forEach(function(property) {
 	document.write(property.name + '<br>' + ' Height: ' + property.height + '<br>' + ' Type: ' + property.types + '<br><br>');
  });
