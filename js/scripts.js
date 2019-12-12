e328c795434ddbe691f65b21cfd6fc5ab89bea44
chrishall71 <chrishallsrq@gmail.com>
2019-12-11 20:16:06 -0600

    Fetch Pokemon external API added
    I submitted it but i am not getting nothing to show in the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,83 @@
 var pokemonRepository = (function () { //wrapped repository Array inside IIFE
-	var repository = [
-		{
-			name: 'Bulbasaur', 
-			height: 2.4,
-			types: ['grass', 'poison']
-		},
-		{
-			name: 'Charmander', 
-			height: 2.0,
-			types: ['fire']
-		},
-		{
-			name: 'Charizard', 
-			height: 5.7,
-			types: ['fire', 'flying']
-		},
-	];
-	
-	function add (pokemon) {
-		repository.push (pokemon);
-	}
-//function to pull all Pokemon data
-	function getall() {
-		return repository;
-	}
+	var repository = [];
+	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+		
+
+	function loadList() {
+	return fetch(apiUrl).then(function (response) {
+		return response.json();
+	}).then(function (json) {
+		json.results.forEach(function (item) {
+			var pokemon = {
+			name: item.name,
+			detailsUrl: item.url
+			};
+			add(pokemon);
+		});
+	}).catch(function (e) {
+		console.error(e);
+	})
+
+	function loadDetails(item) {
+		var url = item.detailsUrl;
+		return fetch(url).then(function (response) {
+		  return response.json();
+		}).then(function (details) {
+		  // Now we add the details to the item
+		  item.imageUrl = details.sprites.front_default;
+		  item.height = details.height;
+		  item.types = Object.keys(details.types);
+		}).catch(function (e) {
+		  console.error(e);
+		});
+	 }
+
 
 	function addListItem (pokemon) {
 		var $pokemonList = document.querySelector('.pokemon-list');
-
-		
-		var $listItem = document.createElement ('li'); //create a li element that contains a button for each Pokemon
-		var $listButton = document.createElement('button');  //creates the button element
-		$listButton.innerText = pokemon.name; //set the innerText of the burtton to be the pokemons name
-		$listButton.classList.add('button-style'); //Add a class tot he button using the classList.add method
-		
-		$listItem.appendChild($listButton);	//append the button to the list item as its child.
-		$pokemonList.appendChild($listItem); //append the child in the repository
+		var $listItem = document.createElement ('li'); 
+		var $listButton = document.createElement('button'); 
+		$listButton.innerText = pokemon.name; 
+		$listButton.classList.add('button-style'); 
+		$listItem.appendChild($listButton);
+		$pokemonList.appendChild($listItem); 
 		$listButton.addEventListener('click', function(event) {
 			showDetails(pokemon); //creatning the button as function/event to be able to click in the future
 		});
 	}
-		function showDetails(pokemon) {
-			console.log (pokemon);
-		}
+
+	function showDetails(item) {
+		pokemonRepository.loadDetails(item).then(function() {
+		console.log (item);
+		});
+	
+	function add(item) {
+		repository.push(item)
+	}
+	function getAll() {
+		return repository;
+	}
 
 	return {
 		add: add,
 		getall: getall,
-		addListItem: addListItem
+		search: search,
+		loadList: loadList,
+		loadDetails: loadDetails
 	};
 })();
 
-pokemonRepository.getall().forEach(function(pokemon){
+pokemonRepository.loadList().then(function() {
+	// Now the data is loaded!
+	pokemonRepository.getAll().forEach(function(pokemon){
+	  addListItem(pokemon);
+	});
+ });
+
+
+//pokemonRepository.getall().forEach(function(pokemon){
 	//document.write(pokemon.name + '<br>' + ' Height: ' + pokemon.height + ' Type: ' + pokemon.types + '<br>');
 
-	pokemonRepository.addListItem(pokemon);
-	var $listButton = document.querySelector(`.${pokemon.name}`);	
- });
+	//pokemonRepository.addListItem(pokemon);
+	//var $listButton = document.querySelector(`.${pokemon.name}`);	
+ //});
